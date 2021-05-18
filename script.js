46f0a49659576d03c3034ddad4b9126122db7d39
Glitch (hello-webpage) <none>
2021-05-18 03:36:18 +0000

    🌵👕 Checkpoint ./script.js:3856783/1184 ./sketch.js:3856783/1534 ./index.html:3856783/11

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+console.log("running")
--- sketch.js
+++ sketch.js
@@ -4,6 +4,27 @@ function setup() {
 
 function draw() {
   background(230);
-  // Delete the slashes on the next line to "uncomment" the ellipse
-  // ellipse(50, 50, 50, 50)
+  // Color + brush settings
+  noFill()
+  strokeWeight(7)
+
+  // Blue circle
+  stroke(30, 120, 230)
+  ellipse(100, 100, 100, 100)
+
+  // Yellow circle
+  stroke("gold")
+  ellipse(157, 150, 100, 100)
+
+  // Black circle
+  stroke(0, 0, 0)
+  ellipse(214, 100, 100, 100)
+
+  // Green circle
+  stroke("forestGreen")
+  ellipse(271, 150, 100, 100)
+
+  // Red circle
+  stroke(230, 30, 30)
+  ellipse(328, 100, 100, 100)
 }
